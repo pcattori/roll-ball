eacf43c903ffd34576f38bc0505e55516b6ad376
Pedro Cattori <pcattori@gmail.com>
2015-05-27 00:14:11 +0200

    Fixed collision bug: error when colliding with non-players

--- unity/Assets/Scripts/Player2Controller.cs
+++ unity/Assets/Scripts/Player2Controller.cs
@@ -23,27 +23,17 @@ public class Player2Controller : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision other) {
-		// use mass ratio to compute velocity transform
-		//this.transform.GetComponent<Rigidbody>().velocity = Vector3.Reflect(other.relativeVelocity*-1, other.contacts[0].normal );
-		/*int bounciness = 1;
-		
-		Vector3 normal = Vector3.zero;
-		foreach(ContactPoint c in collision.contacts) {
-			normal += c.normal;
+		// Elastic collision: v_i' = 2v_cm - v_i
+		if (other.gameObject.layer == 8) {
+			float m_1 = rb.mass;
+			float m_2 = other.gameObject.GetComponent<Rigidbody> ().mass;
+			
+			Vector3 v_1 = velocity;
+			Vector3 v_2 = velocity + other.relativeVelocity;
+			
+			Vector3 v_cm = (m_1 * v_1 + m_2 * v_2) / (m_1 + m_2);
+			
+			rb.velocity = 2f * v_cm - v_1;
 		}
-		normal.Normalize();
-		Vector3 newVelocity = bounciness * (-2f * (Vector3.Dot(velocity,normal) * normal) + velocity);
-		rb.velocity = newVelocity;
-		*/
-
-		float m_1 = rb.mass;
-		float m_2 = other.gameObject.GetComponent<Rigidbody>().mass;
-		
-		Vector3 v_1 = velocity;
-		Vector3 v_2 = velocity + other.relativeVelocity;
-		
-		Vector3 v_cm = (m_1 * v_1 + m_2 * v_2) / (m_1 + m_2);
-		
-		rb.velocity = 2f * v_cm - v_1;
 	}
 }
