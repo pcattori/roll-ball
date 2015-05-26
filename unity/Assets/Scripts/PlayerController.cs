879c0d82993d0c502879242561595d80ea3e8a86
Pedro Cattori <pcattori@gmail.com>
2015-05-27 00:26:38 +0200

    Consolidated player controllers

--- unity/Assets/Scripts/PlayerController.cs
+++ unity/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player1Controller : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
 
 	private Rigidbody rb;
 	private Vector3 velocity;
-	public float speed;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis("Player1Horizontal");
-		float moveVertical = Input.GetAxis("Player1Vertical");
+		float moveHorizontal = Input.GetAxis(rb.tag + "Horizontal");
+		float moveVertical = Input.GetAxis(rb.tag + "Vertical");
 
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
