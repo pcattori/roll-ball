d59ae1eef8169ff1645ba1ee0e5e0cfdefa82c71
Pedro Cattori <pcattori@gmail.com>
2015-05-27 19:34:07 +0200

    Bomb and spring spawning

--- unity/Assets/Scripts/Exploder.cs
+++ unity/Assets/Scripts/Exploder.cs
@@ -13,26 +13,23 @@ public class Exploder : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.layer == 8) { // players
-			Debug.Log("Trigger!");
 			Explode ();
 		}
 	}
 
 	IEnumerator Countdown() {
 		yield return new WaitForSeconds(countdown);
-		Debug.Log ("Time's up!");
 		Explode ();
 	}
 
 	void Explode() {
-		Debug.Log ("explosion");
 		var explosionPos = transform.position;
 		var colliders = Physics.OverlapSphere(explosionPos, radius);
 		foreach (Collider hit in colliders) {
 			if (hit.gameObject.layer == 8) { // players
 				Rigidbody rb = hit.GetComponent<Rigidbody>();
 				if (rb != null) {
-					rb.AddExplosionForce(power * 100, explosionPos, radius, 0.35F);
+					rb.AddExplosionForce(power, explosionPos, radius, 0.35F, ForceMode.Impulse);
 				}
 			}
 		}
---
+++ unity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject item_prefab;
+	public float radius;
+	public float height;
+	public float delay;
+	
+	void Start () {
+		StartCoroutine ("Spawn");
+	}
+
+	IEnumerator Spawn() {
+		while (true) {
+			Vector2 point = Random.insideUnitCircle * radius;
+			GameObject item = Instantiate (item_prefab, new Vector3 (point.x, height, point.y), Quaternion.identity) as GameObject;
+			yield return new WaitForSeconds(delay);
+			Destroy (item);
+		}
+	}
+
+}
